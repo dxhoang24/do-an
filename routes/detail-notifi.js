036673b2e1492320f79d832210e2257959592ce9
hoangdx <daoxuanhoang@hoasao.com.vn>
2024-01-15 17:26:28 +0700

    upcode ngay 15/01

--- routes/detail-notifi.js
+++ routes/detail-notifi.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var detail_notifi = express.Router();
+var nodemailer = require('nodemailer');
 const contacts = require("../models/contact");
 
 detail_notifi.get("/detail-notification/:id", (req, res) => {
@@ -9,5 +10,30 @@ detail_notifi.get("/detail-notification/:id", (req, res) => {
     });
   });
 });
-
+detail_notifi.post("/sendmail", (req, res) => {
+  console.log("req",req.body);
+  let sub = req.body.type=='lienhe' ? 'Phản hồi liên hệ' :'Thông tin đơn hàng'
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'daohoang240620@gmail.com',
+      pass: 'fzot ftdy sxsz qsjy'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'daohoang240620@gmail.com',
+    to: req.body.email,
+    subject: sub,
+    text: req.body.noidung
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent success');
+    }
+  });  
+});
 module.exports = detail_notifi;
